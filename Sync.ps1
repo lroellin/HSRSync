927bda6e5dafb1f79c186ea00fb4004825416975
lukas.roellin <lukas.roellin@bluewin.ch>
2016-10-08 16:34:02 +0200

    Added toast notification

--- Sync.ps1
+++ Sync.ps1
@@ -37,6 +37,7 @@ Write-Success -Message "Mapped."
 $ModulePaths = "Informatik\Fachbereich\Algorithmen_und_Datenstrukturen_2", "Informatik\Fachbereich\C++", "Informatik\Fachbereich\Computernetze_1", "Informatik\Fachbereich\Informations-_und_Codierungstheorie"
 
 Write-Step -Message "Syncing..."
+$StartTime = Get-Date
 Foreach($ModulePath in $ModulePaths) {
     # Using only last part of module path for the local name
     $Name = Split-Path -Path $ModulePath -Leaf
@@ -55,3 +56,25 @@ Foreach($ModulePath in $ModulePaths) {
 Write-Success -Message "Synced."
 
 Remove-PSDrive $NetworkDriveLetter
+
+$EndTime = Get-Date
+$TimeDifference = New-TimeSpan -Start $StartTime -End $EndTime
+
+# Notification
+# by https://gist.github.com/altrive/72594b8427b2fff16431
+$Notification = "HSR Sync`r`nSynced in $($TimeDifference.Seconds)s"
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+#Convert to .NET type for XML manipuration
+$toastXml = [xml] $template.GetXml()
+$toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($Notification)) > $null
+#Convert back to WinRT type
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($toastXml.OuterXml)
+$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+$toast.Tag = "PowerShell"
+$toast.Group = "PowerShell"
+$toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5)
+#$toast.SuppressPopup = $true
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+$notifier.Show($toast);
